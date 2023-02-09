9bab29312a8de9b273ed972bedee046deb1c0958
bridgemami <mdbridgeman@gmail.com>
2023-02-08 17:52:53 -0800

    worked on styling for the about and contact section

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         I earned an associate degree in full-stack web development in December
         2022 from Santa Rosa Junior College. In addition, I have been taking
         classes at BitWise Industries. When I am not coding, I enjoy hiking,
-        running, yoga, traveling, and <em>Star Wars</em>.
+        running, yoga, traveling and <em>Star Wars</em>.
       </p>
     </StyledAboutContainer>
   );
